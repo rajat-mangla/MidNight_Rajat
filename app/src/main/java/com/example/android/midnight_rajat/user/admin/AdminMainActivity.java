904a469621fe5c5d654091432198797abd2c49b4
rajat-mangla <mangla.1@iitj.ac.in>
2017-01-22 01:37:31 +0530

    ADDED VIEWPAGER WITH TABS

--- app/src/main/java/com/example/android/midnight_rajat/user/admin/AdminMainActivity.java
+++ app/src/main/java/com/example/android/midnight_rajat/user/admin/AdminMainActivity.java
@@ -1,15 +1,101 @@
 package com.example.android.midnight_rajat.user.admin;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.example.android.midnight_rajat.R;
 
-public class AdminMainActivity extends AppCompatActivity {
+import static com.example.android.midnight_rajat.user.admin.AdminMainActivity.NUM_PAGES;
 
+public class AdminMainActivity extends FragmentActivity {
+
+
+    public static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+
+        viewPager = (ViewPager)findViewById(R.id.mainpager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(0);
+        }
+    }
+
+
+
+
+
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            return new AddItemsFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return "ORDERS";
+                case 1:
+                    return "FOODDATA";
+                case 2:
+                    return "PROFILE";
+                default:
+                    return "MC-BC";
+            }
+        }
+    }
+
+
 }
+
+
+
